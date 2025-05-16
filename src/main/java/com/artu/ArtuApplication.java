7fd82d31dd076f57a7ee5adfab36f2e9da9c743e
HBY <hbigpillar@gmail.com>
2025-05-16 16:48:35 +0900

    start

---
+++ src/main/java/com/artu/ArtuApplication.java
@@ -0,0 +1,13 @@
+package com.artu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artu/ArtuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
